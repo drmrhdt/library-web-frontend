30aa73ceed9cb24a02d5d0598de1d72b9e1d6f29
drmrhdt <dspopo17@mts.ru>
2020-09-12 15:50:29 +0300

    feat(search): add search moduke and component

--- library-web-frontens/src/app/app.module.ts
+++ library-web-frontens/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { SideMenuModule } from './side-menu/side-menu.module';
+import { SearchModule } from './search/search.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SideMenuModule],
+  imports: [BrowserModule, AppRoutingModule, SideMenuModule, SearchModule],
   providers: [],
   bootstrap: [AppComponent],
 })
