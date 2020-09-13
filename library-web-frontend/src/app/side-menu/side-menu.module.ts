4db427c8d2c8d59263b02b6cf58ea70b1fec3fc3
drmrhdt <drmrhdt@gmail.com>
2020-09-14 00:13:15 +0300

    feat(side-menu.*): add modals for creating vault, book

--- library-web-frontend/src/app/side-menu/side-menu.module.ts
+++ library-web-frontend/src/app/side-menu/side-menu.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { ButtonModule } from '../shared/components/button/button.module';
+import { ButtonModule } from '../shared/components/button/button.module'
+import { ModalModule } from '../shared/components/modal/modal.module'
 
-import { SideMenuComponent } from './side-menu/side-menu.component';
+import { SideMenuComponent } from './side-menu/side-menu.component'
 
 @NgModule({
-  declarations: [SideMenuComponent],
-  imports: [CommonModule, RouterModule, ButtonModule],
-  exports: [SideMenuComponent],
+    declarations: [SideMenuComponent],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        ModalModule
+    ],
+    exports: [SideMenuComponent]
 })
 export class SideMenuModule {}
--- library-web-frontend/src/app/side-menu/side-menu/side-menu.component.ts
+++ library-web-frontend/src/app/side-menu/side-menu/side-menu.component.ts
@@ -1,12 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
-import { menu } from '../side-menu/mocks/menu';
+import { menu } from '../side-menu/mocks/menu'
+import { vaults } from '../../vault/mocks/vaults'
 
 @Component({
-  selector: 'app-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss'],
+    selector: 'app-side-menu',
+    templateUrl: './side-menu.component.html',
+    styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent {
-  menu = menu;
+export class SideMenuComponent implements OnInit {
+    menu = menu
+    vaults = vaults
+    isShowModalDialog = false
+    isCreatingVault = false
+    isCreatingBook = false
+    vaultForm: FormGroup
+    bookForm: FormGroup
+
+    currentVault = null
+
+    constructor(private _formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.vaultForm = this._formBuilder.group({
+            name: ['', [Validators.required, Validators.minLength(2)]],
+            description: ['', Validators.minLength(2)],
+            numShelfs: [1, [Validators.required, Validators.min(1)]],
+            numRows: [1, [Validators.required, Validators.min(1)]]
+            // listOfBooks
+        })
+
+        this.bookForm = this._formBuilder.group({
+            name: ['', [Validators.required, Validators.minLength(2)]],
+            author: ['', [Validators.required, Validators.minLength(2)]],
+            description: ['', Validators.minLength(2)],
+            vault: ['none'],
+            shelf: [''],
+            row: [''],
+            number: ['']
+            // frontCover
+            // sideCover
+        })
+
+        this.setVaultsFields()
+    }
+
+    setVaultsFields(): void {
+        this.bookForm.get('vault').valueChanges.subscribe(id => {
+            this.currentVault = this.vaults.find(vault => vault.id === id)
+            this.bookForm
+                .get('shelf')
+                .patchValue(this.currentVault.shelfs.shelfs[0].number)
+
+            this.bookForm
+                .get('row')
+                .patchValue(this.currentVault.shelfs.shelfs[0].rows[0].number)
+        })
+    }
+
+    toggleModal(type?: string): void {
+        this.isShowModalDialog = !this.isShowModalDialog
+        if (type === 'vault') {
+            this.isCreatingVault = !this.isCreatingVault
+        } else if (type === 'book') {
+            this.isCreatingBook = !this.isCreatingBook
+        }
+    }
+
+    addVault(): void {
+        if (this.vaultForm.valid) {
+            console.log(this.vaultForm.value)
+            this.vaultForm.reset({
+                name: '',
+                description: '',
+                numShelfs: 1,
+                numRows: 1
+            })
+        }
+    }
+
+    addBook(): void {
+        if (this.bookForm.valid) {
+            console.log(this.bookForm.value)
+            this.bookForm.reset({
+                name: '',
+                author: '',
+                description: '',
+                vault: 'none',
+                shelf: '',
+                row: '',
+                number: ''
+            })
+        }
+    }
 }
