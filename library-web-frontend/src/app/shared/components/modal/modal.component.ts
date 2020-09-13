9edb4bfdcba9777039d74c46458524db393f615d
drmrhdt <drmrhdt@gmail.com>
2020-09-14 00:12:36 +0300

    feat(modal): add modal module and component

--- library-web-frontend/src/app/shared/components/modal/modal.component.ts
+++ library-web-frontend/src/app/shared/components/modal/modal.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    Output,
+    Input,
+    TemplateRef,
+    EventEmitter
+} from '@angular/core'
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {}
+export class ModalComponent {
+    @Input() isShowCloseBtn: boolean = true
+    @Input() modalTitle: string = ''
+    @Input() bodyComponent: TemplateRef<any>
+
+    @Output() close: EventEmitter<null> = new EventEmitter<null>()
+
+    closeDialog(): void {
+        this.close.emit()
+    }
+}
