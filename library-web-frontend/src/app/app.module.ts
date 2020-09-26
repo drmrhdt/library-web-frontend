b72c90970fbbb96141ec382686a5addb5775184a
drmrhdt <drmrhdt@gmail.com>
2020-09-26 13:53:38 +0300

    style(app.module.ts): format file

--- library-web-frontend/src/app/app.module.ts
+++ library-web-frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
-import { SideMenuModule } from './side-menu/side-menu.module';
-import { SearchModule } from './search/search.module';
+import { SideMenuModule } from './side-menu/side-menu.module'
+import { SearchModule } from './search/search.module'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SideMenuModule, SearchModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, SideMenuModule, SearchModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
