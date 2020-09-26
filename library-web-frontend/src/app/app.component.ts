86f8f3219b62d4f8d895179997c2396599acfde8
drmrhdt <drmrhdt@gmail.com>
2020-09-26 13:49:52 +0300

    feat(app.component.ts): get vaults and send to app.service

--- library-web-frontend/src/app/app.component.ts
+++ library-web-frontend/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+import { AppService } from './services/app.service'
+import { vaults } from '../assets/mock/vaults'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+    vaults = vaults
+
+    constructor(private _appService: AppService) {
+        this._appService.vaults$.next(vaults)
+    }
+}
