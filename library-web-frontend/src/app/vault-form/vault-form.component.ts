7c0bf9a5f6b304aac52589226ff69f9ced03a912
drmrhdt <drmrhdt@gmail.com>
2021-01-03 18:25:55 +0300

    feat(vault-form.component.ts): add creating vault

--- library-web-frontend/src/app/vault-form/vault-form.component.ts
+++ library-web-frontend/src/app/vault-form/vault-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
+import { VaultService } from '../api/index'
+import { CreateVaultDto } from '../api/model/createVaultDto'
+
 @Component({
     selector: 'app-vault-form',
     templateUrl: './vault-form.component.html',
@@ -9,7 +12,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 export class VaultFormComponent implements OnInit {
     vaultForm: FormGroup
 
-    constructor(private _formBuilder: FormBuilder) {}
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _vaultService: VaultService
+    ) {}
 
     ngOnInit(): void {
         this.vaultForm = this._formBuilder.group({
@@ -27,6 +33,13 @@ export class VaultFormComponent implements OnInit {
             return
         }
 
+        const vaultData: CreateVaultDto = this.vaultForm.value
+        this._vaultService.vaultControllerCreate(vaultData).subscribe(res => {
+            this._resetForm()
+        })
+    }
+
+    private _resetForm() {
         this.vaultForm.reset({
             name: '',
             description: '',
