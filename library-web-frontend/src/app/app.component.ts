57188ea38179664cca632ba6b235270556d19a48
drmrhdt <drmrhdt@gmail.com>
2021-01-03 19:45:23 +0300

    feat(app.component.ts): move constructuring vaults to backend

--- library-web-frontend/src/app/app.component.ts
+++ library-web-frontend/src/app/app.component.ts
@@ -19,42 +19,8 @@ export class AppComponent {
             .bookControllerGetAll()
             .subscribe(books => this._appService.books$.next(books))
 
-        this._vaultService.vaultControllerGetAll().subscribe(vaults => {
-            const unfilteredVaults = vaults
-            const filteredVaults = []
-
-            for (
-                let curVaultNum = 0;
-                curVaultNum < unfilteredVaults.length;
-                curVaultNum++
-            ) {
-                const curVault = unfilteredVaults[curVaultNum]
-                curVault.shelfs = []
-                for (
-                    let curShelf = 1;
-                    curShelf <= curVault.numShelfs;
-                    curShelf++
-                ) {
-                    const shelf = { rows: [] }
-                    // filter books by shelfs
-                    const booksOnCurShelf = curVault.books.filter(
-                        book => book.shelf === curShelf
-                    )
-                    // filter books by rows on current shelf
-                    for (let curRow = 1; curRow <= curVault.numRows; curRow++) {
-                        const booksOnCurRow = booksOnCurShelf
-                            .filter(book => book.row === curRow)
-                            .sort(book => book.number) // sort by order on row
-                        const row = { books: [] }
-                        row.books.push(...booksOnCurRow)
-                        shelf.rows.push(row)
-                    }
-                    curVault.shelfs.push(shelf)
-                }
-                filteredVaults.push(curVault)
-            }
-
-            this._appService.vaults$.next(filteredVaults)
-        })
+        this._vaultService
+            .vaultControllerGetAll()
+            .subscribe(vaults => this._appService.vaults$.next(vaults))
     }
 }
