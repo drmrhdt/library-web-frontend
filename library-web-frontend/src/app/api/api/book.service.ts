61eb3705e055d10e624be4d38323cc41392ec556
drmrhdt <drmrhdt@gmail.com>
2021-01-15 00:27:47 +0300

    feat(api): generate swagger

--- library-web-frontend/src/app/api/api/book.service.ts
+++ library-web-frontend/src/app/api/api/book.service.ts
@@ -18,6 +18,7 @@ import { CustomHttpUrlEncodingCodec }                        from '../encoder';
 import { Observable }                                        from 'rxjs';
 
 import { CreateBookDto } from '../model/createBookDto';
+import { UpdateBookDto } from '../model/updateBookDto';
 
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
 import { Configuration }                                     from '../configuration';
@@ -208,6 +209,57 @@ export class BookService {
 
         return this.httpClient.request<any>('get',`${this.basePath}/v1/api/book/${encodeURIComponent(String(id))}`,
             {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * Update book by id
+     * 
+     * @param body 
+     * @param id 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public bookControllerUpdate(body: UpdateBookDto, id: number, observe?: 'body', reportProgress?: boolean): Observable<any>;
+    public bookControllerUpdate(body: UpdateBookDto, id: number, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<any>>;
+    public bookControllerUpdate(body: UpdateBookDto, id: number, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<any>>;
+    public bookControllerUpdate(body: UpdateBookDto, id: number, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        if (body === null || body === undefined) {
+            throw new Error('Required parameter body was null or undefined when calling bookControllerUpdate.');
+        }
+
+        if (id === null || id === undefined) {
+            throw new Error('Required parameter id was null or undefined when calling bookControllerUpdate.');
+        }
+
+        let headers = this.defaultHeaders;
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected != undefined) {
+            headers = headers.set('Content-Type', httpContentTypeSelected);
+        }
+
+        return this.httpClient.request<any>('patch',`${this.basePath}/v1/api/book/${encodeURIComponent(String(id))}`,
+            {
+                body: body,
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
                 observe: observe,
--- library-web-frontend/src/app/api/model/models.ts
+++ library-web-frontend/src/app/api/model/models.ts
@@ -1,5 +1,6 @@
 export * from './createBookDto';
 export * from './createTagDto';
 export * from './createVaultDto';
+export * from './updateBookDto';
 export * from './updateTagDto';
 export * from './updateUserDto';
