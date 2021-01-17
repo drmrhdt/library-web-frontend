ccf53305913d0ab44e82272e3c4c0ee3e0ceedaa
drmrhdt <drmrhdt@gmail.com>
2021-01-17 16:18:18 +0300

    feat(books): add basic importing books from excel file

--- library-web-frontend/src/app/books/books.component.ts
+++ library-web-frontend/src/app/books/books.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+
+import { mergeMap } from 'rxjs/operators'
+
+import * as XLSX from 'xlsx'
 
 import { AppService } from '../services/app.service'
-import { BookService } from '../api/api/book.service'
-import { mergeMap } from 'rxjs/operators'
-import { VaultService } from '../api/api/vault.service'
+import { BookService, VaultService } from '../api/index'
 
 @Component({
     selector: 'app-books',
@@ -15,6 +17,9 @@ export class BooksComponent implements OnInit {
     book
     isDeletingDialogOpened = false
     isUpdatingDialogOpened = false
+    isExcelFile = false
+
+    @ViewChild('inputFile') inputFile: ElementRef
 
     constructor(
         private _appService: AppService,
@@ -50,5 +55,67 @@ export class BooksComponent implements OnInit {
     toggleDeletingDialog(book): void {
         this.isDeletingDialogOpened = !this.isDeletingDialogOpened
         this.book = book
+    }
+
+    private importBooksToDB(books): void {
+        this._bookService
+            .bookControllerCreate(books)
+            .pipe(
+                mergeMap(() => this._bookService.bookControllerGetAll()),
+                mergeMap(res => {
+                    this._appService.books$.next(res)
+                    return this._vaultService.vaultControllerGetAll()
+                })
+            )
+            .subscribe(res => this._appService.vaults$.next(res))
+    }
+
+    onChange(event): void {
+        let data
+        const target: DataTransfer = <DataTransfer>event.target
+        this.isExcelFile = !!target.files[0].name.match(/(.xls|.xlsx)/)
+        if (target.files.length > 1) {
+            this.inputFile.nativeElement.value = ''
+        }
+        if (!this.isExcelFile) {
+            this.inputFile.nativeElement.value = ''
+            return
+        }
+        //   this.spinnerEnabled = true;
+        const reader: FileReader = new FileReader()
+        reader.onload = (e: any) => {
+            /* read workbook */
+            const bstr: string = e.target.result
+            const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' })
+
+            /* grab first sheet */
+            const wsname: string = wb.SheetNames[0]
+            const ws: XLSX.WorkSheet = wb.Sheets[wsname]
+
+            /* save data */
+            data = XLSX.utils.sheet_to_json(ws)
+        }
+
+        reader.readAsBinaryString(target.files[0])
+
+        reader.onloadend = e => {
+            // this.spinnerEnabled = false;
+
+            data.forEach(book => {
+                const tags = book.tags.split(',')
+                const tagObjects = []
+                tags.forEach(bookTag => {
+                    const tagObject = this._appService.tags$.value.find(
+                        tag => tag.name === bookTag
+                    )
+                    if (tagObject) {
+                        tagObjects.push(tagObject)
+                    }
+                })
+                book.tags = tagObjects
+            })
+
+            this.importBooksToDB(data)
+        }
     }
 }
