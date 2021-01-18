c688ec2a7237a24250e5e3287d80adac7a589897
drmrhdt <drmrhdt@gmail.com>
2021-01-19 01:22:57 +0300

    feat(utils): add startWithOne parameter to getArrayFromNumber()

--- library-web-frontend/src/app/util/util.ts
+++ library-web-frontend/src/app/util/util.ts
@@ -1,3 +1,12 @@
-export function getArrayFromNumber(number: number): number[] {
-    return Array.apply(null, { length: number }).map(Number.call, Number)
+export function getArrayFromNumber(
+    number: number,
+    withNumbersAsElements = false,
+    startWithOne = false
+): number[] {
+    const array = Array.apply(null, { length: number }).map(Number.call, Number)
+    if (!withNumbersAsElements) {
+        return array
+    }
+
+    return array.map((el, index) => (el = startWithOne ? index + 1 : index))
 }
