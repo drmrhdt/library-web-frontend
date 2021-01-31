5692e163c1ebac2ab486f9fee705fb9dc7df02b1
drmrhdt <drmrhdt@gmail.com>
2021-02-01 00:48:46 +0300

    feat: display book's info

--- library-web-frontend/src/app/books/book-details/book-details.component.ts
+++ library-web-frontend/src/app/books/book-details/book-details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+
+import { Subject } from 'rxjs'
+import { takeUntil } from 'rxjs/operators'
+
+import { AppService } from '../../services/app.service'
 
 @Component({
-  selector: 'app-book-details',
-  templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.scss']
+    selector: 'app-book-details',
+    templateUrl: './book-details.component.html',
+    styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
+    book
 
-  constructor() { }
+    private _unsubscriber$ = new Subject()
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private _route: ActivatedRoute,
+        private _appService: AppService
+    ) {}
 
+    ngOnInit(): void {
+        this._appService.books$
+            .pipe(takeUntil(this._unsubscriber$))
+            .subscribe(books => {
+                this.book = books.find(
+                    book => book.id === +this._route.snapshot.params.id
+                )
+            })
+    }
 }
